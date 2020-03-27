699fb36ebc198a9413770683b3bbf58c0cb38f54
Maylee <mgagnon987@gmail.com>
2020-03-27 13:34:45 -0400

    Branch Maylee

--- src/main/java/edu/wpi/teamF/Main.java
+++ src/main/java/edu/wpi/teamF/Main.java
@@ -2,8 +2,14 @@ package edu.wpi.teamF;
 
 public class Main {
 
+  public static void mayleeBranch() {
+    System.out.println("Branch - Maylee");
+  }
+
   public static void main(String[] args) {
     System.out.println("Hello, world!");
     System.out.println("Maylee");
+
+    mayleeBranch();
   }
 }
